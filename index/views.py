6cf2cf772dfb62fa0031ca602fa46d8239cdb8c4
Fabrice Hafashimana <tumbafabruce@gmail.com>
2023-07-05 10:27:35 +0200

    Feat: Refined design

--- index/views.py
+++ index/views.py
@@ -3,11 +3,10 @@ from .models import Product
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
-
 # Create your views here.
 def index(request):
-    products = Product.objects.filter(published=True)[:3]
-    return render(request, "index.html", {'products': products})
+    products = Product.objects.filter(published=True)[:5]
+    return render(request, "index.html", {"products": products})
 
 
 def contact(request):
@@ -19,10 +18,10 @@ def about(request):
 
 
 def shop(request):
-    product_list = Product.objects.filter(published=True)
+    product_list = Product.objects.filter(published=True).order_by("published_date")
     paginator = Paginator(product_list, 10)  # Show 10 products per page
 
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     try:
         products = paginator.page(page)
     except PageNotAnInteger:
@@ -32,9 +31,9 @@ def shop(request):
         # If page is out of range (e.g. 9999), deliver the last page of results
         products = paginator.page(paginator.num_pages)
 
-    return render(request, "products.html", {'products': products})
+    return render(request, "products.html", {"products": products})
 
 
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    return render(request, "single-product.html", {'product': product})
+    return render(request, "single-product.html", {"product": product})
